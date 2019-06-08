b2052fe804f11f6518698f7dd556205162b05dc4
David Coldeira <dcoldeira@gmail.com>
2019-06-09 00:25:42 +0100

    First commit

---
+++ Clicking.lua
@@ -0,0 +1,66 @@
+
+function love.load()
+  button = {}
+  button.x = 200
+  button.y = 200
+  button.size = 50
+
+  score = 0
+  timer = 10
+  gameState = 1
+  myfont = love.graphics.newFont(30)
+
+
+end
+
+function love.update(dt)
+  if gameState == 2 then
+      if timer > 0 then
+        timer = timer - dt
+      end
+
+      if timer < 0 then
+       timer = 0
+       gameState = 1
+       score = 0
+    end
+  end
+end
+
+function love.draw()
+  if gameState == 2 then
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", button.x, button.y, button.size)
+  end
+
+  love.graphics.setFont(myfont)
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.print("Score: ".. score)
+  love.graphics.print("Time ".. math.ceil(timer), 350, 0)
+
+  if gameState == 1 then
+    love.graphics.printf("Click anywhere to begin!", 0, love.graphics.getHeight()/3, love.graphics.getWidth(), "center")
+  end
+
+
+end
+
+function love.mousepressed(x, y, b)
+  if b == 1 and gameState == 2 then
+    if distanceBet(button.x, button.y, love.mouse.getX(), love.mouse.getY()) < button.size then
+      score = score + 1
+      button.x = math.random(button.size, love.graphics.getWidth() - button.size)
+      button.y = math.random(button.size, love.graphics.getHeight() - button.size)
+
+    end
+  end
+  if gameState == 1 then
+    gameState = 2
+    timer = 10
+
+  end
+end
+
+function distanceBet(x1, y1, x2, y2)
+  return math.sqrt((y2 - y1)^2 + (x2 - x1)^2)
+end
